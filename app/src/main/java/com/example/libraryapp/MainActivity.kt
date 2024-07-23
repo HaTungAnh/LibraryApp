481db4e7a65904b2b245f6f16ac47bb23f2e4ca3
HaTungAnh <21021453@vnu.edu.vn>
2024-07-23 21:53:02 +0700

    feat(remember user)

--- app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -11,18 +11,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.libraryapp.navigation.NavigationAppBar
 import com.example.libraryapp.navigation.NavigationGraph
 import com.example.libraryapp.ui.theme.LibraryAppTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        auth = Firebase.auth
+
         setContent {
             LibraryAppTheme {
                 NavigationGraph()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            setContent {
+                LibraryAppTheme {
+                    NavigationAppBar()
+                }
             }
         }
     }
